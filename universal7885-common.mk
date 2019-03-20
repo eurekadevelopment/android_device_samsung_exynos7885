1dcddcc8ecea9f0e9d2c7469b34ad3f6164748ee
SamarV-121 <samarvispute121@gmail.com>
2019-03-20 08:46:02 +0000

    Init trees
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

---
+++ universal7885-common.mk
@@ -0,0 +1,15 @@
+# Screen density
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+# Bluetooth
+PRODUCT_PACKAGES += \
+    audio.a2dp.default
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2340
+TARGET_SCREEN_WIDTH := 1080
+
+# Net
+PRODUCT_PACKAGES += \
+    netutils-wrapper-1.0
