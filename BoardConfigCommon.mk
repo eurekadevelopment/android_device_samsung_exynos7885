57ebd07be6e1560ae5f263017cbacc36b1505d37
Jan Altensen <info@stricted.net>
2020-08-16 01:24:11 +0200

    universal7904: Add slsi wpa_supplicant_8_lib to CFI paths
    Change-Id: Ib2bf001d0d104eeb2540682fd15c2df7dcf1518b
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -88,3 +88,4 @@ BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
 BOARD_HOSTAPD_DRIVER             := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
 WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
+PRODUCT_CFI_INCLUDE_PATHS += hardware/samsung_slsi/scsc_wifibt/wpa_supplicant_lib
