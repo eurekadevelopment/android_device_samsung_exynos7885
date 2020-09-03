2e848bfd4d0e78910fbc60d91a8ab2e892c2535b
SamarV-121 <samarvispute121@gmail.com>
2020-09-03 05:12:18 +0000

    universal7904: Import stock audio policy config
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>
    Change-Id: I017e01d91f5404ef46258eb4d1b3604a28276bd7

--- universal7904-common.mk
+++ universal7904-common.mk
@@ -5,6 +5,10 @@ $(call inherit-product-if-exists, vendor/samsung/universal7904-common/universal7
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio_policy_configuration.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/audio_policy_configuration.xml
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     audio.a2dp.default
