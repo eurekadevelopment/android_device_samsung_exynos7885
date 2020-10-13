476fa29b9e7fee434e967514f09d82457a90b9e4
SamarV-121 <samarvispute121@gmail.com>
2020-10-13 12:36:01 +0000

    universal7885: Remove libstagefright_foundation.so from prebuilt vndk-29
     * F linker : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.media.omx@1.0-service": cannot locate symbol "_ZN7android24MEDIA_MIMETYPE_AUDIO_IMAE" referenced by "/vendor/lib/libstagefright_omx_utils.so"
     * This symbol does exist in /vendor/lib/libstagefright_foundation.so
     * Fixes media playback
    
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- rootdir/bin/sys.sh
+++ rootdir/bin/sys.sh
@@ -1,3 +1,4 @@
 #!/sbin/sh
 echo "$(grep -v libgui.so system/system/system_ext/apex/com.android.vndk.v29/etc/vndkprivate.libraries.29.txt)" > system/system/system_ext/apex/com.android.vndk.v29/etc/vndkprivate.libraries.29.txt
 echo libcamera_client.so >> system/system/system_ext/apex/com.android.vndk.v29/etc/vndkcore.libraries.29.txt
+rm -f system/system/system_ext/apex/com.android.vndk.v29/lib/libstagefright_foundation.so
