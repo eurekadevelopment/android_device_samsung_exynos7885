97c512a212f77a612c51fdfccc3e940b7546f2d9
SamarV-121 <samarvispute121@gmail.com>
2020-10-16 11:31:14 +0000

    universal7904: Decommonize dtbhtoolExynos
     * also add m30lte, a30, a40 in guard
    
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- Android.mk
+++ Android.mk
@@ -1,6 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter m20lte, $(TARGET_DEVICE)),)
+ifneq ($(filter m20lte m30lte a40 a30, $(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
