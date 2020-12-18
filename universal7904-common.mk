e479b444d7aa7b68a86a003bebad1dcca434b7c4
SamarV-121 <samarvispute121@gmail.com>
2020-12-18 14:53:33 +0530

    universal7904: Use skip_mount.cfg to skip /product mounting
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- universal7904-common.mk
+++ universal7904-common.mk
@@ -58,6 +58,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     SamsungDoze
 
+# Skip Mount
+PRODUCT_COPY_FILES += \
+    build/target/product/gsi/gsi_skip_mount.cfg:system/system_ext/etc/init/config/skip_mount.cfg
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
