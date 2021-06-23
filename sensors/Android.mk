0d6b84791b570b9dd3c66ab0bf8621c7dc74e7e5
SamarV-121 <samarvispute121@gmail.com>
2021-06-23 12:54:14 +0530

    universal7904: Compile sensor HAL and audio_policy_config to vendor_overlay/30 as well
    Change-Id: I9322e8d396c7c76efd4ee64cc1aeba1acab94c06
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- sensors/Android.mk
+++ sensors/Android.mk
@@ -34,4 +34,39 @@ LOCAL_POST_INSTALL_CMD += ; mkdir -p $(LOCAL_MODULE_PATH_64)/hw
 LOCAL_POST_INSTALL_CMD += ; ln -sf $(LOCAL_MODULE).so $(LOCAL_MODULE_PATH_32)/hw/android.hardware.sensors@1.0-impl.so
 LOCAL_POST_INSTALL_CMD += ; ln -sf $(LOCAL_MODULE).so $(LOCAL_MODULE_PATH_64)/hw/android.hardware.sensors@1.0-impl.so
 
+LOCAL_REQUIRED_MODULES := android.hardware.sensors@1.0-impl_vndk-30.samsung-universal7904
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := android.hardware.sensors@1.0-impl_vndk-30.samsung-universal7904
+LOCAL_MODULE_TAGS  := optional
+LOCAL_VENDOR_MODULE := true
+
+LOCAL_MODULE_PATH_32 := $(TARGET_OUT_PRODUCT)/vendor_overlay/30/lib
+LOCAL_MODULE_PATH_64 := $(TARGET_OUT_PRODUCT)/vendor_overlay/30/lib64
+LOCAL_MODULE_RELATIVE_PATH := hw
+
+LOCAL_SRC_FILES := \
+    Sensors.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+    liblog \
+    libcutils \
+    libhardware \
+    libhidlbase \
+    android.hardware.sensors@1.0
+
+LOCAL_STATIC_LIBRARIES := \
+    libbase \
+    libutils \
+    android.hardware.sensors@1.0-convert \
+    multihal
+
+LOCAL_POST_INSTALL_CMD := mkdir -p $(LOCAL_MODULE_PATH_32)/hw
+LOCAL_POST_INSTALL_CMD += ; mkdir -p $(LOCAL_MODULE_PATH_64)/hw
+LOCAL_POST_INSTALL_CMD += ; ln -sf $(LOCAL_MODULE).so $(LOCAL_MODULE_PATH_32)/hw/android.hardware.sensors@1.0-impl.so
+LOCAL_POST_INSTALL_CMD += ; ln -sf $(LOCAL_MODULE).so $(LOCAL_MODULE_PATH_64)/hw/android.hardware.sensors@1.0-impl.so
+
 include $(BUILD_SHARED_LIBRARY)
--- universal7904-common.mk
+++ universal7904-common.mk
@@ -9,7 +9,8 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 TARGET_EXCLUDES_AUDIOFX := true
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio_policy_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/etc/audio_policy_configuration.xml
+    $(LOCAL_PATH)/configs/audio_policy_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/etc/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/configs/audio_policy_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/etc/audio_policy_configuration.xml
 
 # Bluetooth
 PRODUCT_PACKAGES += \
