4ef67b7b4413680408e741c6b762e6b0d465b88f
SamarV-121 <samarvispute121@gmail.com>
2021-06-27 16:20:23 +0530

    universal7885: Compile fingerprint HAL from source
    Change-Id: I0c0f3344c90acba572acd27138be8bd213ee21d2
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- universal7904-common.mk
+++ universal7904-common.mk
@@ -45,6 +45,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.fastcharge@1.0-service.samsung
 
+# Figerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service.samsung
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
