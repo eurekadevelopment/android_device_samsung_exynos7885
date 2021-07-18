3df8d124a5a80cf57bc2a3cc29b536959c822d04
SamarV-121 <samarvispute121@gmail.com>
2021-07-18 21:45:12 +0530

    universal7904: Run shfmt
    Change-Id: I835dccd7143af8a6d6ab2012cd37822db4fe0d97
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -9,37 +9,37 @@ set -e
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
+if [[ ! -d ${MY_DIR} ]]; then MY_DIR="${PWD}"; fi
 
 LINEAGE_ROOT="${MY_DIR}"/../../..
 
 HELPER="${LINEAGE_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
-    echo "Unable to find helper script at ${HELPER}"
-    exit 1
+	echo "Unable to find helper script at ${HELPER}"
+	exit 1
 fi
 source "${HELPER}"
 
 function blob_fixup {
-    case "$1" in
-        vendor/lib*/libhifills.so)
-            "$PATCHELF" --add-needed "libunwindstack.so" "$2"
-            ;;
-        vendor/lib*/hw/camera.exynos7904.so)
-            "$PATCHELF" --replace-needed "libcamera_client.so" "libcamera_metadata_helper.so" "$2"
-            "$PATCHELF" --replace-needed "libgui.so" "libgui_vendor.so" "$2"
-            ;;
-        vendor/lib*/libexynoscamera.so|vendor/lib*/libexynoscamera3.so)
-            "$PATCHELF" --remove-needed "libcamera_client.so" "$2"
-            "$PATCHELF" --remove-needed "libgui.so" "$2"
-            ;;
-        vendor/lib*/libsensorlistener.so)
-            "$PATCHELF" --add-needed "libshim_sensorndkbridge.so" "$2"
-            ;;
-        vendor/lib*/libwrappergps.so|vendor/lib/hw/audio.primary.exynos7904.so)
-            "$PATCHELF" --replace-needed "libvndsecril-client.so" "libsecril-client.so" "$2"
-            ;;
-    esac
+	case "$1" in
+	vendor/lib*/libhifills.so)
+		"$PATCHELF" --add-needed "libunwindstack.so" "$2"
+		;;
+	vendor/lib*/hw/camera.exynos7904.so)
+		"$PATCHELF" --replace-needed "libcamera_client.so" "libcamera_metadata_helper.so" "$2"
+		"$PATCHELF" --replace-needed "libgui.so" "libgui_vendor.so" "$2"
+		;;
+	vendor/lib*/libexynoscamera.so | vendor/lib*/libexynoscamera3.so)
+		"$PATCHELF" --remove-needed "libcamera_client.so" "$2"
+		"$PATCHELF" --remove-needed "libgui.so" "$2"
+		;;
+	vendor/lib*/libsensorlistener.so)
+		"$PATCHELF" --add-needed "libshim_sensorndkbridge.so" "$2"
+		;;
+	vendor/lib*/libwrappergps.so | vendor/lib/hw/audio.primary.exynos7904.so)
+		"$PATCHELF" --replace-needed "libvndsecril-client.so" "libsecril-client.so" "$2"
+		;;
+	esac
 }
 
 # Default to sanitizing the vendor folder before extraction
@@ -51,50 +51,51 @@ SECTION=
 KANG=
 
 while [ "${#}" -gt 0 ]; do
-    case "${1}" in
-        --only-common )
-                ONLY_COMMON=true
-                ;;
-        --only-target )
-                ONLY_TARGET=true
-                ;;
-        -n | --no-cleanup )
-                CLEAN_VENDOR=false
-                ;;
-        -k | --kang )
-                KANG="--kang"
-                ;;
-        -s | --section )
-                SECTION="${2}"; shift
-                CLEAN_VENDOR=false
-                ;;
-        * )
-                SRC="${1}"
-                ;;
-    esac
-    shift
+	case "${1}" in
+	--only-common)
+		ONLY_COMMON=true
+		;;
+	--only-target)
+		ONLY_TARGET=true
+		;;
+	-n | --no-cleanup)
+		CLEAN_VENDOR=false
+		;;
+	-k | --kang)
+		KANG="--kang"
+		;;
+	-s | --section)
+		SECTION="${2}"
+		shift
+		CLEAN_VENDOR=false
+		;;
+	*)
+		SRC="${1}"
+		;;
+	esac
+	shift
 done
 
 if [ -z "${SRC}" ]; then
-    SRC="adb"
+	SRC="adb"
 fi
 
 # Initialize the helper for common device
 if [ -z "${ONLY_TARGET}" ]; then
-    # Initialize the helper for common device
-    setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${LINEAGE_ROOT}" true "${CLEAN_VENDOR}"
+	# Initialize the helper for common device
+	setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${LINEAGE_ROOT}" true "${CLEAN_VENDOR}"
 
-    extract "${MY_DIR}/proprietary-files.txt" "${SRC}" \
-            "${KANG}" --section "${SECTION}"
+	extract "${MY_DIR}/proprietary-files.txt" "${SRC}" \
+		"${KANG}" --section "${SECTION}"
 fi
 
 if [ -z "${ONLY_COMMON}" ] && [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
-    # Reinitialize the helper for device
-    source "${MY_DIR}/../${DEVICE}/extract-files.sh"
-    setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}" false "${CLEAN_VENDOR}"
+	# Reinitialize the helper for device
+	source "${MY_DIR}/../${DEVICE}/extract-files.sh"
+	setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}" false "${CLEAN_VENDOR}"
 
-    extract "${MY_DIR}/../${DEVICE}/proprietary-files.txt" "${SRC}" \
-            "${KANG}" --section "${SECTION}"
+	extract "${MY_DIR}/../${DEVICE}/proprietary-files.txt" "${SRC}" \
+		"${KANG}" --section "${SECTION}"
 fi
 
 "${MY_DIR}/setup-makefiles.sh"
