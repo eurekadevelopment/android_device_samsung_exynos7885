0a0b654afa4f2ed2a81968175acb0247a4870779
SamarV-121 <samarvispute121@pm.me>
2021-10-16 10:13:34 +0530

    universal7885: Uprev audio HAL to 7.0
    Change-Id: Ib62f81c407d1e140d5501878d025deb2ee47a73b
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>

--- universal7885-common/universal7885-common.mk
+++ universal7885-common/universal7885-common.mk
@@ -8,9 +8,9 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 TARGET_EXCLUDES_AUDIOFX := true
 
 PRODUCT_PACKAGES += \
-    android.hardware.audio@6.0-impl \
+    android.hardware.audio@7.0-impl \
     android.hardware.audio.service \
-    android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio.effect@7.0-impl \
     libtinycompress \
     audio.r_submix.default \
     audio.usb.default \
