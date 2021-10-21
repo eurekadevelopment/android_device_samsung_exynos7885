SamarV-121/android_device_samsung_universal7904-common

universal7904: Enable ZRAM
Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

universal7904-common.mk
@@ -19,7 +19,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Init scripts
 PRODUCT_PACKAGES += \
-    init.target.rc
+    init.target.rc \
+    fstab.enableswap
 
 # Net
 PRODUCT_PACKAGES += \

eurekadevelopment/android_device_samsung_exynos7885

Change-Id: Icd48749ad781ba48592fbcc2dc7c3ad88c659a57

universal7904-common.mk
@@ -19,7 +19,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Init
 PRODUCT_PACKAGES += \
-    init.universal7904.rc
+    init.universal7904.rc \
+    fstab.enableswap
 
 # Net
 PRODUCT_PACKAGES += \