9b3f0e2b09368c784e37fdc4f93cf270c64e1f5c
roynatech2544 <whiteshell2544@naver.com>
2021-10-21 23:06:54 +0900

    universal7885: commonize bluetooth include header
    Change-Id: I5f2a893d9ae588bd30dd80bdc346b07dda498459
    Signed-off-by: roynatech2544 <whiteshell2544@naver.com>

--- universal7885-common/bluetooth/bdroid_buildcfg.h
+++ universal7885-common/bluetooth/bdroid_buildcfg.h
@@ -1,6 +1,28 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
+#pragma push_macro("PROPERTY_VALUE_MAX")
+
+#include <cutils/properties.h>
+#include <string.h>
+
+static inline const char* BtmGetDefaultName()
+{
+    char product_device[PROPERTY_VALUE_MAX];
+    property_get("ro.product.device", product_device, "");
+
+
+    if (strstr(product_device, "a20e"))
+        return "Galaxy A20e";
+    if (strstr(product_device, "a20"))
+        return "Galaxy A20";
+
+    // Fallback to Moto SDM Generic
+    return "Samsung Galaxy";
+}
+
+#define BTM_DEF_LOCAL_NAME BtmGetDefaultName()
 #define BTM_ESCO_TRANSPORT_UNIT_SIZE_PCM16
 
+#pragma pop_macro("PROPERTY_VALUE_MAX")
 #endif
