3076a0b90bbb5e375ca5d22f1655018f7fc92331
Soo-Hwan Na <69458352+roynatech2544@users.noreply.github.com>
2021-10-24 13:56:21 +0900

    common: update redfin fingerprint to latest

--- universal7885-common/fingerprint.mk
+++ universal7885-common/fingerprint.mk
@@ -1,7 +1,7 @@
 PRODUCT_GMS_CLIENTID_BASE := android-google
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "google/redfin/redfin:12/SPB5.210812.002/7671067:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:12/SP1A.210812.015/7679548:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-        PRIVATE_BUILD_DESC="redfin-user 12 SPB5.210812.002 7671067 release-keys"
+        PRIVATE_BUILD_DESC="redfin-user 12 SP1A.210812.015 7679548 release-keys"
