c7f80dea23c5a4cc08d6cb73a676b9322a38734f
roynatech2544 <whiteshell2544@naver.com>
2021-10-26 14:42:01 +0900

    universal7885: Set correct board name
    * It fixes sensor module not loading
    
    Change-Id: I784c29465908f29e8eaa1cdb8f56b82a0c3cab4a
    Signed-off-by: roynatech2544 <whiteshell2544@naver.com>

--- universal7885-common/universal7885-common.mk
+++ universal7885-common/universal7885-common.mk
@@ -261,9 +261,6 @@ PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libcutils-v29.so \
     prebuilts/vndk/v29/arm64/arch-arm-armv8-a/shared/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_VENDOR)/lib/libcutils-v29.so
 
-# VNDK
-PRODUCT_EXTRA_VNDK_VERSIONS := 30
-
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
