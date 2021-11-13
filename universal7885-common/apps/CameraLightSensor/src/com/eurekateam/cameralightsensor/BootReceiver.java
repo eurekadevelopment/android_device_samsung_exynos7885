e4569f7c82daadf014f68147aa1976819bb6ee69
roynatech2544 <whiteshell2544@naver.com>
2021-11-13 19:19:44 +0900

    universal7885: CameraLightSensor: Use correct code to start service
    * W ActivityManager: Unable to start service Intent { cmp=com.eurekateam.cameralightsensor/.CameraLightSensorService } U=0: not found
    
    * Ref: https://stackoverflow.com/questions/19105587/unable-to-start-service-intent-not-found
    
    * Makes BootReceiver unable to start service

--- universal7885-common/apps/CameraLightSensor/src/com/eurekateam/cameralightsensor/BootReceiver.java
+++ universal7885-common/apps/CameraLightSensor/src/com/eurekateam/cameralightsensor/BootReceiver.java
@@ -3,15 +3,14 @@ package com.eurekateam.cameralightsensor;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import com.eurekateam.cameralightsensor.CameraLightSensorService;
 
 public class BootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if (intent.getAction().equalsIgnoreCase(Intent.ACTION_BOOT_COMPLETED)) {
             if (context != null) {
-                final Intent i = new Intent();
-                i.setClassName("com.eurekateam.cameralightsensor",
-                        "com.eurekateam.cameralightsensor.CameraLightSensorService");
+                final Intent i = new Intent(context, CameraLightSensorService.class);
                 context.startForegroundService(i);
             }
         }
