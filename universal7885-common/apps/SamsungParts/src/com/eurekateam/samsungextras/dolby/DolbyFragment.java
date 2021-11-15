6728e97ab8c2bc94b18738ff99af5e8c0409277b
Soo-Hwan Na <69458352+roynatech2544@users.noreply.github.com>
2021-11-15 23:31:26 +0900

    SamsungParts: address typing mistake

--- universal7885-common/apps/SamsungParts/src/com/eurekateam/samsungextras/dolby/DolbyFragment.java
+++ universal7885-common/apps/SamsungParts/src/com/eurekateam/samsungextras/dolby/DolbyFragment.java
@@ -43,7 +43,7 @@ public class DolbyFragment extends PreferenceFragment implements
         CharSequence[] items = {
             "1", "2", "3", "4", "5", "6", "7", "8", "9"
         };
-        DolbyModesPreference.setEntries(items);
+        DolbyModesPreference.setEntryValues(items);
         DolbyModesPreference.setOnPreferenceChangeListener((preference, newValue) -> {
             switch (Integer.parseInt((String) newValue)){
                 case 1:
