7d5e7a19a6027dd1197b4638263275767a894b9a
roynatech2544 <whiteshell2544@naver.com>
2021-11-16 00:46:39 +0900

    SamsungParts: Dolby: More interactive

--- universal7885-common/apps/SamsungParts/src/com/eurekateam/samsungextras/DolbyTile.java
+++ universal7885-common/apps/SamsungParts/src/com/eurekateam/samsungextras/DolbyTile.java
@@ -1,18 +1,33 @@
 package com.eurekateam.samsungextras;
 
+import android.service.quicksettings.Tile;
 import android.service.quicksettings.TileService;
 
 import com.eurekateam.samsungextras.dolby.DolbyCore;
 
 public class DolbyTile extends TileService {
+    private boolean isRunning;
+    DolbyCore dolbyCore = new DolbyCore();
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+        isRunning = dolbyCore.isRunning();
+        updateTile();
+    }
+
     @Override
     public void onClick() {
-        DolbyCore dolbyCore = new DolbyCore();
-        if(dolbyCore.isRunning()){
+        if (isRunning) {
             dolbyCore.stopDolbyEffect();
         }else{
             dolbyCore.justStartOnly();
         }
+        isRunning = !isRunning;
+        updateTile();
     }
-
+    private void updateTile() {
+        final Tile tile = getQsTile();
+        tile.setState(isRunning ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+        tile.updateTile();
+    }
 }
