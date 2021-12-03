2176d171f9b23c4c1aa297be691510e95b3ff378
Soo-Hwan Na <69458352+roynatech2544@users.noreply.github.com>
2021-12-03 12:46:47 +0900

    vendorsetup: add more vendor dependencies

--- universal7885-common/vendorsetup.sh
+++ universal7885-common/vendorsetup.sh
@@ -1,12 +1,19 @@
-git clone  git@github.com:eurekadevelopment/android_vendor_samsung_a20_r.git vendor/samsung -b master
-#git clone git@github.com:eurekadevelopment/android_device_samsung_a20_r.git device/samsung
 rm -rf hardware/samsung
 git clone git@github.com:eurekadevelopment/android_hardware_samsung.git hardware/samsung -b NUKE_ALL
-git clone git@github.com:eurekadevelopment/Eureka-Kernel-Exynos7885-Q-R-S-private.git kernel/samsung/exynos7885 --depth=1 -b R8.0_rom
+git clone git@github.com:eurekadevelopment/Eureka-Kernel-Exynos7885-Q-R-S-private.git kernel/samsung/exynos7885 --depth=1 -b R8.1_rom
 git clone https://github.com/lineageos/android_hardware_samsung_nfc hardware/samsung/nfc
 git clone https://github.com/lineageos/android_hardware_samsung_slsi_libbt hardware/samsung_slsi/libbt
 git clone https://github.com/lineageos/android_hardware_samsung_slsi_scsc_wifibt_wifi_hal hardware/samsung_slsi/scsc_wifibt/wifi_hal
 git clone https://github.com/lineageos/android_hardware_samsung_slsi_scsc_wifibt_wpa_supplicant_lib hardware/samsung_slsi/scsc_wifibt/wpa_supplicant_lib
+# Vendors
+mkdir -p tmp
+git clone git@github.com:eurekadevelopment/android_vendor_samsung_a20_r.git --depth=1 tmp/
+git clone git@github.com:eurekadevelopment/proprietary_vendor_samsung_a40_r.git tmp/a40 --depth=1
+git clone git@github.com:eurekadevelopment/proprietary_vendor_samsung_a30_r.git tmp/a30 --depth=1
+git clone git@github.com:eurekadevelopment/android_vendor_samsung_a10_arm64_R.git tmp/a10 --depth=1
+git clone git@github.com:eurekadevelopment/proprietary_vendor_samsung_universal7904-common_r.git --depth=1
+rm -rf vendor/samsung
+mv tmp vendor/samsung
 if test -f device/samsung/universal7885-common/vendor_name; then
 rm device/samsung/universal7885-common/vendor_name
 fi
