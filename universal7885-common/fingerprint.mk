ea4f13167b52866c9472154d61cd222301e32f48
Soo-Hwan Na <69458352+roynatech2544@users.noreply.github.com>
2021-12-08 18:17:00 +0900

    universal7885: fingerprint: Move to redfin

--- universal7885-common/fingerprint.mk
+++ universal7885-common/fingerprint.mk
@@ -4,4 +4,4 @@ PRODUCT_GMS_CLIENTID_BASE := android-google
 BUILD_FINGERPRINT := "google/redfin/redfin:12/SQ1A.211205.008/7888514:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-        PRIVATE_BUILD_DESC="raven-user 12 SQ1A.211205.008 7888514 release-keys"
+        PRIVATE_BUILD_DESC="redfin-user 12 SQ1A.211205.008 7888514 release-keys"
