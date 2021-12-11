4bc4078a13de3b9b979eb280aa30bdc4889b249e
roynatech2544 <whiteshell2544@naver.com>
2021-12-11 18:37:38 +0900

    a10: update vendor pathes for vt
    Signed-off-by: roynatech2544 <whiteshell2544@naver.com>

--- a10/device.mk
+++ a10/device.mk
@@ -3,7 +3,7 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 # Inherit common device configuration
 $(call inherit-product, device/samsung/universal7885-common/universal7885-common.mk)
 
-$(call inherit-product, vendor/samsung/a10/a10-vendor.mk)
+$(call inherit-product, vendor/samsung/a10-arm64/a10-vendor.mk)
 
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 
