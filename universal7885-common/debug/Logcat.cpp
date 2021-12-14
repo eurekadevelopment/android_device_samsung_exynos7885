f679a720b0e4005805269cd5b3ac124be87cd0f2
roynatech2544 <whiteshell2544@naver.com>
2021-12-14 13:38:31 +0900

    universal7885: add cc binary for logging
    Signed-off-by: roynatech2544 <whiteshell2544@naver.com>

---
+++ universal7885-common/debug/Logcat.cpp
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2021 Soo Hwan Na "Royna"
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define CHECK_LOGCAT "/data/debug/logcat"
+#define WRITE_LOGCAT "/data/debug/logs/logcat.txt"
+#define LOG_TAG "DebugLogcatDaemon"
+
+#include <fstream> 
+#include <log/log.h>
+#include <iostream>
+
+bool check_data(){
+    std::ifstream datafile(CHECK_KMSG);
+    if(datafile.good()){
+    	datafile.close();
+        ALOGI("Found %s, logging...", CHECK_KMSG);
+        return true;
+    }else{
+        ALOGW("Couldn't find %s, not logging...", CHECK_KMSG);
+        return false;
+    }
+}
+
+void copy_logcat(){
+    if(check_data()){
+    	system("/system/bin/logcat -f /data/debug/logs/logcat.txt");
+    }
+}
+
