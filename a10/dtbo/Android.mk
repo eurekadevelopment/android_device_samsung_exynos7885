af5231541612627e795652c46ed28392b08a2b9d
Gabriel2392 <gabriel824m@gmail.com>
2022-01-07 16:46:00 +0000

    universal7885-common: add condition for copying dtbo image

--- a10/dtbo/Android.mk
+++ a10/dtbo/Android.mk
@@ -1,2 +1,4 @@
+ifeq ($(TARGET_DEVICE),a10)
 LOCAL_PATH := $(call my-dir)
 $(call add-radio-file,eureka_dtbo.img)
+endif
--- a20/dtbo/Android.mk
+++ a20/dtbo/Android.mk
@@ -1,2 +1,4 @@
+ifeq ($(TARGET_DEVICE),a20)
 LOCAL_PATH := $(call my-dir)
 $(call add-radio-file,eureka_dtbo.img)
+endif
