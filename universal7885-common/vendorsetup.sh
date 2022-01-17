e3c2965d51f3de40e89111c1a558dd86c7bf9282
Kevios12 <kerschbaumer.kevin2@gmail.com>
2022-01-17 14:55:14 +0000

    universal7885: using https instead of git@git
    Causes publickey issues at cloning...

--- universal7885-common/vendorsetup.sh
+++ universal7885-common/vendorsetup.sh
@@ -1,11 +1,11 @@
 rm -rf hardware/samsung
-git clone git@github.com:eurekadevelopment/Eureka-Kernel-Exynos7885-Q-R-S-private.git kernel/samsung/exynos7885 --depth=1 -b R8.1_rom
+git clone --depth=1 https://github.com/eurekadevelopment/Eureka-Kernel-Exynos7885-Q-R-S-private.git -b R8.1_rom kernel/samsung/exynos7885
 git clone https://github.com/lineageos/android_hardware_samsung_nfc hardware/samsung/nfc
 git clone https://github.com/lineageos/android_hardware_samsung_slsi_libbt hardware/samsung_slsi/libbt
 git clone https://github.com/eurekadevelopment/android_hardware_samsung_slsi_scsc_wifibt_wifi_hal.git hardware/samsung_slsi/scsc_wifibt/wifi_hal
 git clone https://github.com/lineageos/android_hardware_samsung_slsi_scsc_wifibt_wpa_supplicant_lib hardware/samsung_slsi/scsc_wifibt/wpa_supplicant_lib
 # Vendors
-git clone git@github.com:eurekadevelopment/android_vendor_samsung_exynos7885.git --depth=1 vendor/samsung
+git clone --depth=1 https://github.com/eurekadevelopment/android_vendor_samsung_exynos7885.git -b master vendor/samsung
 if test -f device/samsung/universal7885-common/vendor_name; then
 rm device/samsung/universal7885-common/vendor_name
 fi
