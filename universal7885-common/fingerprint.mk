37263b9e76358c2d71ab61a3436c5a98a749c49f
Gabriel <88463214+Gabriel2392@users.noreply.github.com>
2022-02-08 07:30:37 -0300

    universal7885: update fingerprint

--- universal7885-common/fingerprint.mk
+++ universal7885-common/fingerprint.mk
@@ -1,7 +1,7 @@
 PRODUCT_GMS_CLIENTID_BASE := android-google
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "google/raven/raven:12/SQ1D.220105.007/8030436:user/release-keys"
+BUILD_FINGERPRINT := "google/raven/raven:12/SQ1D.220205.003/8069835:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-        PRIVATE_BUILD_DESC="raven-user 12 SQ1D.220105.007 8030436 release-keys"
+        PRIVATE_BUILD_DESC="raven-user 12 SQ1D.220205.003 8069835 release-keys"
