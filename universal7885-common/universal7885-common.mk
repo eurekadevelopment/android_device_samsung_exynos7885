380475026db268c4b8796f1c5db232820c2915fd
Robert Shih <robertshih@google.com>
2022-02-15 17:07:30 +0000

    universal7885: Enable AIDL DRM HALs
    Bug: 219538389
    Test: atest GtsMediaTestCases
    Change-Id: I431554dcbef014f8235f048ee062a218a2131f9c

--- universal7885-common/universal7885-common.mk
+++ universal7885-common/universal7885-common.mk
@@ -109,7 +109,7 @@ PRODUCT_PACKAGES += eklogger
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm-service.clearkey \
     android.hardware.drm@1.4.vendor
 
 # Fingerprint
