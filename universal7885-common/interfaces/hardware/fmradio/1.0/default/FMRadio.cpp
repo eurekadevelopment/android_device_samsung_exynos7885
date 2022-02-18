ebd1dbe38a35b0333dd679dc042377d7d6b53685
roynatech2544 <whiteshell2544@naver.com>
2022-02-18 21:17:47 +0900

    universal7885: interfaces: Add FM Radio HAL
    Change-Id: Iae728e0cb37477c563c7d0b632bf894fb4219444
    Signed-off-by: roynatech2544 <whiteshell2544@naver.com>

---
+++ universal7885-common/interfaces/hardware/fmradio/1.0/default/FMRadio.cpp
@@ -0,0 +1,46 @@
+// Copyright (C) 2021 Eureka Team
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "FMRadio.h"
+#include <fstream>
+#include <iostream>
+#include <sstream>
+namespace vendor::eureka::hardware::fmradio::V1_0 {
+
+Return<void> IFMRadio::setManualFreq(float freq) {
+    std::ofstream file;
+    file.open("/sys/devices/virtual/s610_radio/s610_radio/radio_freq_ctrl");
+    file << freq * 1000;
+    file.close();
+    return Void();
+}
+
+Return<void> IFMRadio::adjustFreqByStep(fmradio::V1_0::Direction dir) {
+    std::ofstream file;
+    std::string value = "";
+    if (dir == Direction::UP){
+    	value = "1 50";
+    } else if (dir == Direction::DOWN){
+    	value = "0 50";
+    }
+    file.open("/sys/devices/virtual/s610_radio/s610_radio/radio_freq_seek");
+    file << value;
+    file.close();
+    return Void();
+}
+
+IFMRadio* FMRadio::getInstance(void) {
+    return new FMRadio();
+}
+}  // namespace vendor::eureka::hardware::fmradio::V1_0
