0fde9697c99ce97aceadfc02e269755e5363f124
Soo-Hwan Na <69458352+roynatech2544@users.noreply.github.com>
2022-03-23 07:22:06 +0900

    universal7885: Drop non existing copy files

--- universal7885-common/universal7885-common.mk
+++ universal7885-common/universal7885-common.mk
@@ -274,10 +274,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
-# Seccomp
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
-
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl.samsung \
