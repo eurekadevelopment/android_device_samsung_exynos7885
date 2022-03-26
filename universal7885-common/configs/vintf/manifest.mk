648aeb9509b79b8108814c3a0c3efd6a7ef08c42
roynatech2544 <whiteshell2544@naver.com>
2022-03-26 20:29:32 +0900

    universal7885: Drop sperate vintf for A40
    * No longer needed as commit of vendor repo 'A40: update gnss vendor blob to 2.1' commit
    See: https://github.com/eurekadevelopment/android_vendor_samsung_exynos7885/commit/bd1ce235e9c06e4edc9a4f9e5c4ecddb22e46bfe
    
    Change-Id: Ib4b11611d97f1ccbb75df1f0f22c794628553be0
    Signed-off-by: roynatech2544 <whiteshell2544@naver.com>

--- universal7885-common/configs/vintf/manifest.mk
+++ universal7885-common/configs/vintf/manifest.mk
@@ -4,7 +4,3 @@ DEVICE_MANIFEST_FILE := $(MANIFEST_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(MANIFEST_PATH)/compatibility_matrix.xml
 ODM_MANIFEST_SKUS += NFC
 ODM_MANIFEST_NFC_FILES := $(MANIFEST_PATH)/manifest_nfc.xml
-
-ifneq ($(filter $(TARGET_DEVICE), a40),)
-DEVICE_MANIFEST_FILE += $(MANIFEST_PATH)/manifest_gnss.xml
-endif
