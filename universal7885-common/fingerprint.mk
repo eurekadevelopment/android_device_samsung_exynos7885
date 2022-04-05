1972345bcc064a12577681f798afc0fc7289a60d
kevios12 <kerschbaumer.kevin2@gmail.com>
2022-04-05 10:21:56 +0200

    universal7885: fp: Update to SP2A.220405.004 raven

--- universal7885-common/fingerprint.mk
+++ universal7885-common/fingerprint.mk
@@ -1,7 +1,7 @@
 PRODUCT_GMS_CLIENTID_BASE := android-google
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "google/redfin/redfin:12/SP2A.220305.012/8177914:user/release-keys"
+BUILD_FINGERPRINT := "google/raven/raven:12/SP2A.220405.004/8233519:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-        PRIVATE_BUILD_DESC="redfin-user 12 SP2A.220305.012 8177914 release-keys"
+        PRIVATE_BUILD_DESC="raven-user 12 SP2A.220405.004 8233519 release-keys"
