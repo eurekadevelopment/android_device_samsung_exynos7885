6c43e1c6ebf1df0f46aa0d4a816b1dafb1b6dc4f
roynatech2544 <whiteshell2544@naver.com>
2022-04-30 09:25:25 +0900

    universal7885: Fixup mistakes
    * Set flag for keymaster, import Google hardware Soong namespaces back

--- universal7885-common/universal7885-common.mk
+++ universal7885-common/universal7885-common.mk
@@ -281,6 +281,8 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel \
     hardware/samsung
 
 # Sysconfig
