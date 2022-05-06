4518c616968e3a7b8149bdfcae4201fdcbf5167a
roynatech2544 <whiteshell2544@naver.com>
2022-05-06 12:40:42 +0900

    universal7885: Adapt to DRM hal changes from 9820
    - Update proprietary files list to include drm 1.4 hal
    - Also add missing entries on a10 proprietary files list
    - Build needed interface for drm 1.4 for vendor

--- universal7885-common/universal7885-common.mk
+++ universal7885-common/universal7885-common.mk
@@ -78,7 +78,8 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey \
     android.hardware.drm@1.1.vendor \
     android.hardware.drm@1.2.vendor \
-    android.hardware.drm@1.3.vendor
+    android.hardware.drm@1.3.vendor \
+    android.hardware.drm@1.4.vendor
 
 # Fingerprint
 TARGET_BOARD_HAS_FP ?= true
