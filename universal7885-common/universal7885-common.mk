dbd446b32ca7bdc3248bfcd138d9c2e30161e41e
Tim Zimmermann <tim@linux4.de>
2022-05-27 17:52:15 +0900

    Revert "universal7885: Uprev graphic composer HAL from 2.1 to 2.4"
    * Our HWC does not support setActiveModeWithConstraints
    * This way we can actually have mode switching working
      which will be needed for Tiramisu resolution settings
    
    This reverts commit 6c0bc759058676c59356b70d12ce211a3270db49.

--- universal7885-common/universal7885-common.mk
+++ universal7885-common/universal7885-common.mk
@@ -102,7 +102,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.composer@2.4-service \
+    android.hardware.graphics.composer@2.2-service \
     android.hardware.graphics.mapper@2.0-impl \
     libgui_vendor
 
