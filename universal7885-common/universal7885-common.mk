fa2d822329d53fa99efa901dcf4ba2af654a4e55
Josh Wu <joshwu@google.com>
2022-08-15 21:50:57 +0100

    universal7885: bluetooth: migrate to AIDL audio HAL

--- universal7885-common/universal7885-common.mk
+++ universal7885-common/universal7885-common.mk
@@ -26,7 +26,7 @@ PRODUCT_PACKAGES += \
 
 # Audio (BT)
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth.audio@2.1-impl \
+    android.hardware.bluetooth.audio-impl \
     audio.bluetooth.default
 
 # Audio Configs
