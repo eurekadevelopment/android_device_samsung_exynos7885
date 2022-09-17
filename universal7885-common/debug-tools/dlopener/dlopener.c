ae1c3087bf1a5c74fd275a22c520b2cd62d36fea
roynatech2544 <whiteshell2544@naver.com>
2022-09-17 19:04:18 +0900

    universal7885: dlopener: Format code
    That code formatting looks awful

--- universal7885-common/debug-tools/dlopener/dlopener.c
+++ universal7885-common/debug-tools/dlopener/dlopener.c
@@ -2,27 +2,26 @@
 #include <errno.h>
 #include <stdio.h>
 
-int main(int argc, char *argv[])
-{
-    const char *path;
-    int status = -EINVAL;
-    void *handle = NULL;
+int main(int argc, char *argv[]) {
+  const char *path;
+  int status = -EINVAL;
+  void *handle = NULL;
 
-    if(argc <= 1) {
-        printf("Please specify a module to load!\n");
-        return status;
-    }
-
-    path = argv[1];
-
-    handle = dlopen(path, RTLD_NOW);
-    if (handle == NULL) {
-        char const *err_str = dlerror();
-        printf("load: module=%s\n%s\n", path, err_str?err_str:"unknown");
-        status = -EINVAL;
-    } else {
-        printf("load: module=%s %s\n", path,"Success!");
-    }
-
+  if (argc <= 1) {
+    printf("Please specify a module to load!\n");
     return status;
+  }
+
+  path = argv[1];
+
+  handle = dlopen(path, RTLD_NOW);
+  if (handle == NULL) {
+    char const *err_str = dlerror();
+    printf("load: module=%s\n%s\n", path, err_str ? err_str : "unknown");
+    status = -EINVAL;
+  } else {
+    printf("load: module=%s %s\n", path, "Success!");
+  }
+
+  return status;
 }
