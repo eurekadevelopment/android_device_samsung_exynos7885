bfe2d6397ef4726f17e041a1083c8171ec2367d5
roynatech2544 <whiteshell2544@naver.com>
2022-09-22 10:08:48 +0900

    universal7885: Build missing radio@1.0

--- universal7885-common/universal7885-common.mk
+++ universal7885-common/universal7885-common.mk
@@ -272,6 +272,7 @@ PRODUCT_COPY_FILES += \
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.4.vendor \
+    android.hardware.radio@1.0 \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
     vendor.samsung.hardware.radio@2.1.vendor \
