9f508f12ae2469ad37f44a58df9f4c59d93116c3
roynatech2544 <whiteshell2544@naver.com>
2022-10-05 09:59:35 +0000

    universal7885: RIP hidl-support

--- universal7885-common/hidl-packages/parts/hardware/parts/1.0/default/CachedClass.h
+++
@@ -1,9 +0,0 @@
-template <class C> static inline C *cachedClass(C *cached) {
-  if (cached != nullptr) {
-    return cached;
-  }
-  cached = new C();
-  return cached;
-}
-
-#define USE_CACHED(cache) ({ return cachedClass((cache)); })
