ec1db378bdd250e94025843fc96bdaa9dcee2f5d
roynatech2544 <whiteshell2544@naver.com>
2022-10-22 17:32:33 +0900

    universal7885: fm-aidl: MiddleState: Bounds checking
    we need at least 2 elements on the vector. If it doesn't meet the requirements, return nullptr to keep it as-is unmodified.

--- universal7885-common/apps/aidl-support/fm/default/MiddleState.cpp
+++ universal7885-common/apps/aidl-support/fm/default/MiddleState.cpp
@@ -3,6 +3,12 @@
 middlestate_t *saveMiddleState(const int value, const std::vector<int> &vec)
 {
 	unsigned int i;
+
+	// Bounds checking, we need at least 2 elements on the vector.
+	// If it doesn't meet the requirements, return nullptr to keep it
+	// as-is.
+	if (vec.size() <= 1) return nullptr;
+
 	for (i = 0; i < vec.size() - 1; i++) {
 		const int first = vec[i] - value;
 		const int second = vec[i + 1] - value;
