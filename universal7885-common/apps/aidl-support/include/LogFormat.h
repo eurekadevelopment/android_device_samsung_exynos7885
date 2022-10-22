b5a6677f6058a08aa02c8460116cabd32d6879e9
roynatech2544 <whiteshell2544@naver.com>
2022-10-22 22:56:22 +0900

    universal7885: aidl-support: Add a common header for log format

---
+++ universal7885-common/apps/aidl-support/include/LogFormat.h
@@ -0,0 +1,14 @@
+#include <sstream>
+#include <vector>
+
+#define make_str(a, ...) _make_str(__FILE__, __LINE__, a, ##__VA_ARGS__).c_str()
+
+template <typename... Args>
+std::string _make_str(const std::string& filename, int line, const std::string& fmt, Args... args) {
+  const int size = std::snprintf(nullptr, 0, fmt.c_str(), args...);
+  std::vector<char> buf(size + 1);
+  std::snprintf(buf.data(), buf.size(), fmt.c_str(), args...);
+  std::stringstream ss;
+  ss << "[" << filename << ":" << line << "] " << std::string(buf.begin(), buf.end());
+  return ss.str();
+} 
