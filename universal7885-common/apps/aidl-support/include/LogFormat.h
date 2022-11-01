60659bf10c0cf6b189a1a92a12884e57db60e39d
roynatech2544 <whiteshell2544@naver.com>
2022-11-01 12:31:22 +0900

    universal7885: liblogfmt: IDKIDC -Wformat-security

--- universal7885-common/apps/aidl-support/include/LogFormat.h
+++ universal7885-common/apps/aidl-support/include/LogFormat.h
@@ -5,9 +5,12 @@
 
 template <typename... Args>
 std::string _make_str(const std::string& filename, int line, const std::string& fmt, Args... args) {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wformat-security"
   const int size = std::snprintf(nullptr, 0, fmt.c_str(), args...);
   std::vector<char> buf(size + 1);
   std::snprintf(buf.data(), buf.size(), fmt.c_str(), args...);
+#pragma clang diagnostic pop
   std::stringstream ss;
   ss << "[" << filename << ":" << line << "] " << std::string(buf.begin(), buf.end());
   return ss.str();
