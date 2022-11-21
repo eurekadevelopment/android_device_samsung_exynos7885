b2b0515b288d73fa84f39ace66d8aae6891e2612
roynatech2544 <whiteshell2544@naver.com>
2022-11-21 20:29:09 +0900

    universal7885: parts-jni: Use std::round

--- universal7885-common/apps/SamsungParts/jni/Storage.cpp
+++ universal7885-common/apps/SamsungParts/jni/Storage.cpp
@@ -1,23 +1,16 @@
 #include <filesystem>
 #include <jni.h>
+#include <cmath>
 #include <sys/stat.h>
 
 namespace fs = std::filesystem;
-
-double roundDouble(double origin) {
-  int first = static_cast<int>(origin);
-  double second = origin - first;
-  second = static_cast<int>(second * 10);
-  return static_cast<double>(first *= 10 + second) / 10;
-}
 
 extern "C" JNIEXPORT jdouble JNICALL
 Java_com_eurekateam_samsungextras_interfaces_Swap_getFreeSpace(JNIEnv /*env*/,
                                                                jclass /*clazz*/) {
   fs::space_info data = fs::space("/data");
   auto freespace = data.free / 1024 / 1024;
-  auto result = static_cast<double>(freespace) / 1024;
-  return roundDouble(result);
+  return std::round((static_cast<double>(freespace) / 1024) / 0.1) * 0.1;
 }
 
 extern "C" JNIEXPORT jlong JNICALL
