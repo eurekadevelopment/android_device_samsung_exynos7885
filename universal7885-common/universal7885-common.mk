c1b41684c3cf10a663cd6b4fc6bd93f803639d6b
Gabriel2392 <gabriel824m@gmail.com>
2022-12-20 16:12:06 +0000

    universal7885: fix vintf issues

--- universal7885-common/universal7885-common.mk
+++ universal7885-common/universal7885-common.mk
@@ -80,6 +80,8 @@ PRODUCT_PACKAGES += eklogger
 
 # DRM
 PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service \
     android.hardware.drm@1.3-service.clearkey \
     android.hardware.drm@1.3.vendor
 
