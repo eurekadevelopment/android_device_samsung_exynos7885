00182f48b69c6f3354cc5e967527b8dbe2e80e19
[REDACTED] <60199831+Melyyn@users.noreply.github.com>
2023-09-04 22:17:53 +0100

    Update vendorsetup.sh & minor edit
    - Updated to use default Eureka kenel branch
    - Formatting edit

--- universal7885-common/vendorsetup.sh
+++ universal7885-common/vendorsetup.sh
@@ -1,12 +1,12 @@
 UNIVERSAL="device/samsung/universal7885-common"
 FM_PATH="packages/apps/FMRadio"
 
-git clone --depth=1 https://github.com/eurekadevelopment/Eureka-Kernel-Exynos7885-Q-R-S -b R13_rom kernel/samsung/exynos7885
+git clone --depth=1 https://github.com/eurekadevelopment/Eureka-Kernel-Exynos7885-Q-R-S -b R13-AOSP kernel/samsung/exynos7885
 git clone https://github.com/eurekadevelopment/android_hardware_samsung_slsi_libbt hardware/samsung_slsi/libbt
 git clone https://github.com/eurekadevelopment/android_hardware_samsung_slsi_scsc_wifibt_wifi_hal.git hardware/samsung_slsi/scsc_wifibt/wifi_hal
 git clone https://github.com/lineageos/android_hardware_samsung_slsi_scsc_wifibt_wpa_supplicant_lib hardware/samsung_slsi/scsc_wifibt/wpa_supplicant_lib
 mv hardware/samsung/nfc .
-git clone https://github.com/eurekadevelopment/android_hardware_samsung hardware/samsung -b AOSP-13
+git clone https://github.com/eurekadevelopment/android_hardware_samsung -b AOSP-13 hardware/samsung
 mv nfc hardware/samsung
 git clone --depth=1 https://github.com/eurekadevelopment/android_vendor_samsung_exynos7885.git -b android-13 vendor/samsung
 if test -f ${UNIVERSAL}/vendor_name; then
