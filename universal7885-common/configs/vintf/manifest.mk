3ea997f050b9e685fff11390bb2895d1eaed05d1
roynatech2544 <whiteshell2544@naver.com>
2023-11-27 00:44:25 +0900

    universal7885: Add a30s specific los vintf
    - A30s tsp has hover mode support

--- universal7885-common/configs/vintf/manifest.mk
+++ universal7885-common/configs/vintf/manifest.mk
@@ -1,6 +1,12 @@
 MANIFEST_PATH := device/samsung/universal7885-common/configs/vintf
 
-DEVICE_MANIFEST_FILE := $(MANIFEST_PATH)/manifest.xml $(MANIFEST_PATH)/lineage_manifest.xml
+DEVICE_MANIFEST_FILE := $(MANIFEST_PATH)/manifest.xml
+
+ifeq ($(TARGET_DEVICE),a30s)
+DEVICE_MANIFEST_FILE += $(MANIFEST_PATH)/lineage_manifest_a30s.xml
+else
+DEVICE_MANIFEST_FILE += $(MANIFEST_PATH)/lineage_manifest.xml
+endif
 DEVICE_MATRIX_FILE := $(MANIFEST_PATH)/compatibility_matrix.xml
 ODM_MANIFEST_SKUS += NFC
 ODM_MANIFEST_NFC_FILES := $(MANIFEST_PATH)/manifest_nfc.xml
