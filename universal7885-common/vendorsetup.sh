026fc6857d769e00ab1ba2d65c04a68d0aab570c
roynatech2544 <whiteshell2544@naver.com>
2023-11-28 14:58:16 +0900

    universal7885: makefile_generator: Move it out of host-tools

--- universal7885-common/vendorsetup.sh
+++ universal7885-common/vendorsetup.sh
@@ -8,7 +8,7 @@ fi
 if test -f ${UNIVERSAL}/vendor_name; then
 	rm ${UNIVERSAL}/vendor_name
 fi
-python3 ${UNIVERSAL}/host-tools/makefile_generator.py
+python3 ${UNIVERSAL}/generate_product_makefiles.py
 for dev in a10dd a10 a20 a20e a30 a30s a40; do
 	echo "Generating ${dev} Makefiles..."
 	bash ${UNIVERSAL}/setup.sh "$dev"
