cd81fbf73db78a18cffbfb6f396caa8832077876
roynatech2544 <whiteshell2544@naver.com>
2023-11-30 13:09:07 +0900

    universal7885: vendorsetup: Remove FM Radio rmrf
    - And refactor makefile generator

--- universal7885-common/generate_product_makefiles.py
+++ universal7885-common/generate_product_makefiles.py
@@ -1,11 +1,14 @@
 import os
-blacklist = ["qcom", "nxp","pixel-framework", "lawnchair"  ,"support", "overlay","samsung", "gapps", "google", "gms", "codeaurora", "addons", "overlays", "xtras", "themes"]
-vendors = os.listdir("vendor")
+
+blacklist = ["qcom", "nxp","pixel-framework", "lawnchair" ,"support", "overlay","samsung", "gapps", "google", "gms", "codeaurora", "addons", "overlays", "xtras", "themes"]
+
+vendors = os.listdir('vendor')
+
 for vendor in vendors:
-  common = "vendor/"+vendor+"/config/common.mk"
+  common = f'vendor/{vendor}/config/common.mk'
   if os.path.isfile(common) and vendor not in blacklist:
-     f = open("device/samsung/universal7885-common/vendor_name", "a")
-     f.write(vendor)
-     f.close()
+     print('vendor name is', vendor)
+     with open("device/samsung/universal7885-common/vendor_name", 'w') as f:
+         f.write(vendor)
      break
 
--- universal7885-common/vendorsetup.sh
+++ universal7885-common/vendorsetup.sh
@@ -1,21 +1,11 @@
 UNIVERSAL="device/samsung/universal7885-common"
-FM_PATH="packages/apps/FMRadio"
 
 if [ ! -e .repo/local_manifests/eureka_deps.xml ]; then
 	git clone https://github.com/eurekadevelopment/local_manifests .repo/local_manifests
 	echo "Run repo sync again"
-fi
-if test -f ${UNIVERSAL}/vendor_name; then
-	rm ${UNIVERSAL}/vendor_name
 fi
 python3 ${UNIVERSAL}/generate_product_makefiles.py
 for dev in a10dd a10 a20 a20e a30 a30s a40; do
 	echo "Generating ${dev} Makefiles..."
 	bash ${UNIVERSAL}/setup.sh "$dev"
 done
-
-# Remove multiple declared FMRadio path (we have our own FMRadio and this cause build error)
-if [ -d "$FM_PATH" ]; then
-	echo "Remove FMRadio from ROM Source"
-	rm -Rf $FM_PATH
-fi
