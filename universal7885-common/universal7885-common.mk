d7d6105a8a4c93af34c969e786e22e456aa2e199
SamarV-121 <samar@samarv121.dev>
2023-12-26 15:24:34 +0530

    Revert "universal7885: Uprev audio HAL to 7.1"
    Reason for revert: crashing when try to make a call
    
    This reverts commit 4087eba38fa33d5b9ca911d6037be0a22cd618db.

--- universal7885-common/universal7885-common.mk
+++ universal7885-common/universal7885-common.mk
@@ -17,7 +17,7 @@ PRODUCT_ENFORCE_RRO_TARGETS := framework-res SystemUI Bluetooth
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS := # leave it empty
 
 PRODUCT_PACKAGES += \
-    android.hardware.audio@7.1-impl \
+    android.hardware.audio@7.0-impl \
     android.hardware.audio.service \
     android.hardware.audio.effect@7.0-impl \
     libtinycompress \
