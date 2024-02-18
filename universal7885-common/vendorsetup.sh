9fde391659e264df86c6ea1bf1cd9d888e9dbaaa
Soo Hwan Na <whiteshell2544@naver.com>
2024-02-18 22:51:22 +0900

    universal7885: generate_product_makefiles: Unify in python

--- universal7885-common/vendorsetup.sh
+++ universal7885-common/vendorsetup.sh
@@ -1,11 +1,6 @@
-UNIVERSAL="device/samsung/universal7885-common"
-
 if [ ! -e .repo/local_manifests/eureka_deps.xml ]; then
 	git clone https://github.com/eurekadevelopment/local_manifests .repo/local_manifests
 	echo "Run repo sync again"
 fi
-python3 ${UNIVERSAL}/generate_product_makefiles.py
-for dev in a10dd a10 a20 a20e a30 a30s a40; do
-	echo "Generating ${dev} Makefiles..."
-	bash ${UNIVERSAL}/setup.sh "$dev"
-done
+
+python3 device/samsung/universal7885-common/generate_product_makefiles.py
