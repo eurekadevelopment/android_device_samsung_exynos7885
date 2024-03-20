d1505f85ca411552949956acfb8947e99b198577
EliasOfWaffle <eliascontato@protonmail.com>
2024-03-20 21:58:14 +0000

    a10: Kang Dex configs
    Kanged from https://github.com/Evolution-X-Devices/device_xiaomi_veux/blob/udc/device.mk

--- a10/device.mk
+++ a10/device.mk
@@ -10,5 +10,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.config.art_lowmem=true
 
+# Dex
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := verify
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+USE_DEX2OAT_DEBUG := false
+
 include device/samsung/a10/common.mk
 
