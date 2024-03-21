5bf5dfdf1d0e56ea1d738ffdd4bcd2ba59d050e0
FuriousDroid <furiousdroid@furiousdroid>
2024-03-21 11:45:48 -0400

    universal7885: dtbo: fix race condition

--- universal7885-common/dtbo/Android.mk
+++ universal7885-common/dtbo/Android.mk
@@ -1,21 +1,18 @@
 # BOARD_CUSTOM_DTBOIMG_MK := $(PLATFORM_PATH)/kernel/dtbo.mk
 
-INSTALLED_DTBIMAGE_TARGET := $(PRODUCT_OUT)/eureka_dtbo.img
 MKDTIMG := $(HOST_OUT_EXECUTABLES)/mkdtimg$(HOST_EXECUTABLE_SUFFIX)
-
 KERNEL_OUT := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ
 DTBO_DIR   := $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts/exynos/dtbo
-
 DTBO_CFG := $(COMMON_PATH)/dtbo/$(TARGET_DEVICE).cfg
 
-define build-dtboimage-target
-    $(call pretty,"Target dtbo image: $(INSTALLED_DTBIMAGE_TARGET)")
-    $(MKDTIMG) cfg_create $@ $(DTBO_CFG) -d $(DTBO_DIR)
-    $(hide) chmod a+r $@
-endef
-
-$(INSTALLED_DTBIMAGE_TARGET): $(MKDTIMG) $(INSTALLED_KERNEL_TARGET)
-	$(build-dtboimage-target)
+INSTALLED_DTBIMAGE_TARGET := $(PRODUCT_OUT)/eureka_dtbo.img
+	
+$(INSTALLED_DTBIMAGE_TARGET): $(PRODUCT_OUT)/kernel $(MKDTBOIMG)
+	$(call pretty,"Target dtbo image: $(INSTALLED_DTBIMAGE_TARGET)")
+	$(hide) echo "Building eureka_dtbo.img"
+	$(MKDTIMG) cfg_create $@ $(DTBO_CFG) -d $(DTBO_DIR)
+	$(hide) $(call assert-max-image-size,$@,$(BOARD_DTBIMAGE_PARTITION_SIZE),raw)
+	$(hide) chmod a+r $@
 	
 .PHONY: dtbimage
 dtbimage: $(INSTALLED_DTBIMAGE_TARGET)
