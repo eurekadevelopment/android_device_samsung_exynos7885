4602cdb2977e3d887d18ca9a2475ac6f16d9913d
Rakib Hossain <101888982+iamrh1819@users.noreply.github.com>
2024-04-28 19:11:04 +0600

    Update and rename a10/full_a10.mk to lineage_a10.mk

--- lineage_a10.mk
+++ lineage_a10.mk
@@ -10,9 +10,23 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configuration
 $(call inherit-product, device/samsung/a10/device.mk)
 
+# Inherit some common lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Stuffs
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_BUILD_APERTURE_CAMERA := true
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Maintainer
+RISING_MAINATAINER := Ʀᴀ㉿ɪƁ
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := a10
-PRODUCT_NAME := full_a10
+PRODUCT_NAME := lineage_a10
 PRODUCT_MODEL := SM-A105F
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
