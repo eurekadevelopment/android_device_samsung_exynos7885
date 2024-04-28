b34bd4e8d9ce5eec058c1f20057a3f2088ed6729
Rakib Hossain <101888982+iamrh1819@users.noreply.github.com>
2024-04-28 19:12:50 +0600

    Create lineage_a10.mk

---
+++ a10/lineage_a10.mk
@@ -0,0 +1,32 @@
+# Copyright (C) 2018 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/a10/device.mk)
+
+# Inherit some common lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Stuffs
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_BUILD_APERTURE_CAMERA := true
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Maintainer
+RISING_MAINATAINER := Ʀᴀ㉿ɪƁ
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := a10
+PRODUCT_NAME := lineage_a10
+PRODUCT_MODEL := SM-A105F
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
