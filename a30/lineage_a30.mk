23708afd6eace020fee9a2e887c816c74f6557f6
Rakib Hossain <101888982+iamrh1819@users.noreply.github.com>
2024-04-29 22:26:27 +0600

    Update and rename full_a30.mk to lineage_a30.mk

--- a30/lineage_a30.mk
+++ a30/lineage_a30.mk
@@ -10,9 +10,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configuration
 $(call inherit-product, device/samsung/a30/device.mk)
 
+# Inherit some common rom stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Rom Specific Flags
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Maintainer
+RISING_MAINATAINER := Ʀᴀ㉿ɪƁ
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := a30
-PRODUCT_NAME := full_a30
+PRODUCT_NAME := lineage_a30
 PRODUCT_MODEL := SM-A305F
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
