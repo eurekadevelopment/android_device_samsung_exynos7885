a04c8afc1ff1d17a95930cb9f14b86060bbf3dad
K4LCHAKRA <oseverest44@gmail.com>
2025-03-27 14:25:10 +0000

    universal7885-common: Fix boot image profile path for Soong build
    The Soong build was failing due to a missing boot-image-profile.txt file.
    This change updates PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION
    to point to frameworks/base/boot/boot-image-profile.txt,
    resolving the issue.
    
    Fixes: build errors related to missing boot-image-profile.txt
    Signed-off-by: K4LCHAKRA <oseverest44@gmail.com>

--- universal7885-common/universal7885-common.mk
+++ universal7885-common/universal7885-common.mk
@@ -382,7 +382,7 @@ PRODUCT_SOONG_NAMESPACES += \
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/boot/boot-image-profile.txt
 
 # Task profiles
 PRODUCT_COPY_FILES += \
